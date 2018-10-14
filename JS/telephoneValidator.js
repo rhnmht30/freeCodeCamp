c6b22b5709207bf8a43977f620dda8cc44b49f18
Rohan <rhnmht30@gmail.com>
2018-10-14 18:19:03 +0530

    deleting all files

--- JS/telephoneValidator.js
+++
@@ -1,7 +0,0 @@
-function telephoneCheck(str) {
-    // Good luck!
-    let myregex = /^(1\s?)?(\([0-9]{3}\)|[0-9]{3})[\s\-]?[0-9]{3}[\s\-]?[0-9]{4}$/;
-    return myregex.test(str);
-}
-
-telephoneCheck("555-555-5555");
